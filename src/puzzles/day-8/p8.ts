37fcc841c28150da8c2b87c0f5d2c9c38677388d
Leon Dore <leon.dore@gmail.com>
2022-12-14 23:58:21 -0400

    Complete day 8 part 2

--- src/puzzles/day-8/p8.ts
+++ src/puzzles/day-8/p8.ts
@@ -12,6 +12,7 @@ let grid: { [key: string]: number[] } = Object.fromEntries(input);
 const columns: number = grid[0].length;
 const rows: number = Object.keys(grid).length;
 let visibleTrees = columns * 2 + rows * 2 - 4;
+let highestScore: number = 0;
 
 function getRowRange(side: string, position: Coordinate): number[] {
   const [x, y] = position;
@@ -25,11 +26,7 @@ function getRowRange(side: string, position: Coordinate): number[] {
   return [...Array(amount).keys()].map((index) => grid[index + offset][x]);
 }
 
-function isVisible(
-  side: string,
-  treeHeight: number,
-  position: Coordinate
-): boolean {
+function getSection(side: string, position: Coordinate): number[] {
   const [x, y] = position;
   let trees: number[] = [];
 
@@ -43,22 +40,57 @@ function isVisible(
     trees = getRowRange(side, position);
   }
 
-  return treeHeight > Math.max(...trees);
+  return trees;
+}
+
+function isVisible(side: string, position: Coordinate): boolean {
+  const [x, y] = position;
+  const treeHeight = grid[y][x];
+  return treeHeight > Math.max(...getSection(side, position));
+}
+
+function calculateScenicScore(side: string, position: Coordinate): number {
+  const [x, y] = position;
+  const treeHeight = grid[y][x];
+  const trees = getSection(side, position);
+
+  if (side === 'top' || side === 'left') trees.reverse();
+
+  const blockPosition = trees.findIndex((tree) => tree >= treeHeight);
+
+  if (blockPosition === -1) {
+    return trees.length;
+  }
+
+  return blockPosition + 1;
+}
+
+function calculateFullScore(position: Coordinate): number {
+  const sides = ['top', 'right', 'bottom', 'left'];
+  return sides.reduce(
+    (total: number, side) => total * calculateScenicScore(side, position),
+    1
+  );
 }
 
 for (let y = 1; y < rows - 1; y++) {
   for (let x = 1; x < columns - 1; x++) {
-    if (
-      isVisible('top', grid[y][x], [x, y]) ||
-      isVisible('right', grid[y][x], [x, y]) ||
-      isVisible('bottom', grid[y][x], [x, y]) ||
-      isVisible('left', grid[y][x], [x, y])
-    ) {
-      visibleTrees += 1;
+    // if (
+    //   isVisible('top', [x, y]) ||
+    //   isVisible('right', [x, y]) ||
+    //   isVisible('bottom', [x, y]) ||
+    //   isVisible('left', [x, y])
+    // ) {
+    //   visibleTrees += 1;
+    // }
+
+    const score = calculateFullScore([x, y]);
+    if (score > highestScore) {
+      highestScore = score;
     }
   }
 }
 
-console.log(visibleTrees);
+console.log(highestScore);
 
 export {};
